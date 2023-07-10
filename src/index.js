6be1fdc2fb2c74d4f285a2a2b057800e52136ec6
koloseki <koloseki2002@gmail.com>
2023-07-10 20:14:12 +0200

    Reboot of the project with TS ,Vite and Vitest

--- src/index.js
+++
@@ -1,5 +0,0 @@
-import './style.css'
-import favicons from "favicons";
-import {hi} from "./ships";
-
-hi();
--- src/tests/ships.test.js
+++
@@ -1,5 +0,0 @@
-const hi = require('../ships.js');
-
-test("Test of jest", () => {
-    expect(hi()).toBe("Hello world");
-});
